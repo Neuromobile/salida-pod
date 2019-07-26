fda4a29c895a9999c146ed9eb0f28552d13960c1
Fran Cazorla <fcazorla@neuromobilemarketing.com>
2019-07-26 13:02:51 +0200

    Uploaded Salida [RELEASE] version 1.1.1

--- Salida.framework/git_version.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-set -e
-version=`git describe --tags | cut -d 'v' -f2-`
-echo "Updating NMSVersion.h version to: ${version}"
-sed -i -e "s/sdk_version_placeholder/$version/g" Salida/NMSVersion.h
--- Salida.framework/git_version_post_compile.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-set -e
-git checkout Salida/NMSVersion.h
-rm -f Salida/NMSVersion.h-e
