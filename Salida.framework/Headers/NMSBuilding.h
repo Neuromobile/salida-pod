8c4b90aafad2f2f97aafba347406a6b85ee1f2bd
Fran Cazorla <fcazorla@neuromobilemarketing.com>
2019-12-09 10:36:44 +0100

    Uploaded Salida [RELEASE] version 1.1.8

--- Salida.framework/Headers/NMSBuilding.h
+++ Salida.framework/Headers/NMSBuilding.h
@@ -4,15 +4,15 @@
 
 @interface NMSBuilding : NSObject <NSCoding>
 
-@property (nonatomic, copy) NSNumber *buildingId;
-@property (nonatomic, copy) NSNumber *centerId;
-@property (nonatomic, copy) NSNumber *mainBuildingId;
-@property (nonatomic, copy) NSString *name;
-@property (nonatomic, copy) NSString *type;
-@property (nonatomic, copy) NSDecimalNumber *latitude;
-@property (nonatomic, copy) NSDecimalNumber *longitude;
-@property (nonatomic, copy) NSDecimalNumber *radius;
+@property (nonatomic, strong) NSNumber *buildingId;
+@property (nonatomic, strong) NSNumber *centerId;
+@property (nonatomic, strong) NSNumber *mainBuildingId;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *type;
+@property (nonatomic, strong) NSDecimalNumber *latitude;
+@property (nonatomic, strong) NSDecimalNumber *longitude;
+@property (nonatomic, strong) NSDecimalNumber *radius;
 @property (nonatomic, assign) bool calibrated;
-@property (nonatomic, copy) NSArray<NMSZone *> *zones;
+@property (nonatomic, strong) NSArray<NMSZone *> *zones;
 
 @end
